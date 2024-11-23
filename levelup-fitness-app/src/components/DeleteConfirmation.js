cbfbdae233cb03887969acd70fce0696d5e409f4
Waterhorse1215 <71351223+Waterhorse1215@users.noreply.github.com>
2024-11-23 16:21:57 -0500

    Update DeleteConfirmation.js
    Completed and Implemented into the project. Will show "Would you like to delete this?"

--- levelup-fitness-app/src/components/DeleteConfirmation.js
+++ levelup-fitness-app/src/components/DeleteConfirmation.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const DeleteConfirmation = () => {
+const DeleteConfirmation = ({ onDelete, onCancel }) => {
+  
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  
+  const handleDeleteClick = () => {
+    setShowConfirmation(true);
+  };
+
+  
+  const handleConfirmClick = () => {
+    onDelete(); 
+    setShowConfirmation(false);
+  };
+
+  // Function to handle cancel click
+  const handleCancelClick = () => {
+    setShowConfirmation(false);
+    if (onCancel) {
+      onCancel();  
+    }
+  };
+
   return (
     <div>
-      <h1>Brandon will do this</h1>
+      {/* If confirmation is not shown, display delete button */}
+      {!showConfirmation ? (
+        <div>
+          <button onClick={handleDeleteClick}>Delete Item</button>
+        </div>
+      ) : (
+
+        <div>
+          <h1>Are you sure you want to delete this item?</h1>
+          <button onClick={handleConfirmClick}>Yes, Delete</button>
+          <button onClick={handleCancelClick}>Cancel</button>
+        </div>
+      )}
     </div>
   );
 };
